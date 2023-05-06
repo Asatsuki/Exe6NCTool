e7e6214357bcbd3471fe7b6e5c9aa5b2111ab2a8
Asatsuki <morningmoon256@gmail.com>
2023-05-06 22:43:14 +0900

    描画を追加

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,35 @@
-import { greeting } from './modules/sub';
+import { Color } from "./modules/parts_data";
 
-console.log('Hello, TypeScript');
+const mapCanvas = document.getElementById("mapCanvas") as HTMLCanvasElement;
+const ctx = mapCanvas.getContext("2d") as CanvasRenderingContext2D;
+const gridImg = new Image();
+gridImg.src = "/img/grid.svg";
 
-greeting('Blues.EXE');
+window.onload = () => {
+    draw();
+}
+
+function draw() {
+    const partW = 64;
+    const partH = 64;
+    const mapX = 32;
+    const mapY = 32;
+    const lineWidth = 8;
+
+    ctx.clearRect(0, 0, mapCanvas.width, mapCanvas.height);
+    ctx.drawImage(Color.GREEN.img_pls, mapX + 128, mapY + 128);
+
+    // グリッドを描画
+    for (let i = 0; i < 7; i++) {
+        for (let j = 0; j < 7; j++) {
+            if ((i == 0 || i == 6) && (j == 0 || j == 6)) {
+                continue;
+            }
+            ctx.drawImage(gridImg, mapX + j * partW, mapY + i * partH);
+        }
+    }
+
+    // 境目を描画
+    ctx.fillStyle = "black";
+    ctx.fillRect(mapX + 128, mapY + 128, partW / 2, lineWidth);
+}
--- src/modules/parts_data.ts
+++ src/modules/parts_data.ts
@@ -4,14 +4,24 @@ export interface NCPart {
 }
 
 export class Color {
-    public static WHITE = new Color("white");
-    public static RED = new Color("red");
-    public static BLUE = new Color("blue");
-    public static GREEN = new Color("green");
-    public static YELLOW = new Color("yellow");
-    public static PINK = new Color("pink");
+    public static WHITE = new Color("white", "prg_white.svg", "pls_white.svg");
+    public static RED = new Color("red", "prg_red.svg", "pls_red.svg");
+    public static BLUE = new Color("blue", "prg_blue.svg", "pls_blue.svg");
+    public static GREEN = new Color("green", "prg_green.svg", "pls_green.svg");
+    public static YELLOW = new Color("yellow", "prg_yellow.svg", "pls_yellow.svg");
+    public static PINK = new Color("pink", "prg_pink.svg", "pls_pink.svg");
+
+    public img_prg: HTMLImageElement;
+    public img_pls: HTMLImageElement;
 
     public constructor(
-        public name: string
-    ) { }
+        public name: string,
+        img_prg_name: string,
+        img_pls_name: string,
+    ) {
+        this.img_prg = new Image();
+        this.img_prg.src = `/img/${img_prg_name}`
+        this.img_pls = new Image();
+        this.img_pls.src = `/img/${img_pls_name}`
+    }
 }
