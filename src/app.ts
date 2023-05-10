c1ce323818091e327a34ae4adebe69f44e115916
Asatsuki <morningmoon256@gmail.com>
2023-05-10 21:25:49 +0900

    WebWorker途中
    なぜか帰ってきたClassの関数が実行できない

--- src/app.ts
+++ src/app.ts
@@ -1,66 +1,10 @@
-import { Color, Part, PartUtils, partsData } from "./modules/parts_data";
+import { Constants as c, Color, Part, PartInstance, PlaceSet, PrecalcPart, PartUtils, partsData } from "./modules/parts_data";
+
 import Tagify from '@yaireo/tagify'
-
-class PartInstance {
-    public constructor (
-        public part: Part,
-        public color: Color,
-        public spin: number,
-        public compressed: boolean,
-        public x: number,
-        public y: number
-    ) {}
-
-    public placedMemMap(): boolean[][] | undefined {
-        let commandLine = false; // コマンドラインに1マスでもかかっているかどうか（かかっていないとプログラムパーツが無効）
-        let internal = false; // 中央5x5に1マスでもかかっているかどうか（かかっていないと置けない）
-        const memMap: boolean[][] = Array.from(new Array(mapH), () => new Array(mapW).fill(false));
-        const rotated = PartUtils.getRotatedShape(this.part, this.spin);
-        for (let pY = 0; pY < partH; pY++) {
-            for (let pX = 0; pX < partW; pX++) { // pX, pYはパーツ上の座標
-                const mapX = pX - centerX + this.x; // マップ上の対応座標
-                const mapY = pY - centerY + this.y; // マップ上の対応座標
-                if ((rotated[pY][pX] == 1)) { // この位置にパーツがある
-                    if (mapX < 0 || mapX >= mapW || mapY < 0 || mapY >= mapH) {
-                        // 枠外にはみ出る
-                        return undefined;
-                    } else if ((mapX == 0 || mapX == mapW-1) && (mapY == 0 || mapY == mapH-1)) {
-                        // 四隅を使う
-                        return undefined;
-                    } else {
-                        memMap[mapY][mapX] = true;
-                        if (mapY == 3) commandLine = true; 
-                        if (mapX >= 1 && mapX <= 5 && mapY >= 1 && mapY <= 5) internal = true;
-                    }
-                }
-            }
-        }
-        if (this.part.isProgram && !commandLine) return undefined;
-        if (!internal) return undefined;
-        return memMap;
-    }
-}
-
-interface PlaceSet {
-    memMap: boolean[][]
-    partI: PartInstance
-}
-
-interface PrecalcPart {
-    part: Part,
-    places: PlaceSet[]
-}
-
-
-const mapW = 7;
-const mapH = 7;
-const partW = 5;
-const partH = 5;
-const centerX = 2;
-const centerY = 2;
 
 const mapCanvas = document.getElementById("mapCanvas") as HTMLCanvasElement;
 const ctx = mapCanvas.getContext("2d") as CanvasRenderingContext2D;
+
 const gridImg = new Image();
 gridImg.src = "./img/grid.svg";
 const commandImg = new Image();
@@ -69,6 +13,20 @@ const memMapImg = new Image();
 memMapImg.src = "./img/memory_map.svg";
 const partNameImg = new Image();
 partNameImg.src = "./img/part_name.svg";
+
+const partPrgImgs = new Map<string, HTMLImageElement>();
+const partPlsImgs = new Map<string, HTMLImageElement>();
+(function () {
+    const colors = ["white", "red", "blue", "green", "yellow", "pink"];
+    colors.map((el) => {
+        const partPrgImg = new Image();
+        partPrgImg.src = `./img/prg_${el}.svg`;
+        partPrgImgs.set(el, partPrgImg);
+        const partPlsImg = new Image();
+        partPlsImg.src = `./img/pls_${el}.svg`;
+        partPlsImgs.set(el, partPlsImg);
+    });
+}());
 
 const precalcParts = new Map<string, PrecalcPart>();
 const inputElm = document.querySelector('input[name=tags]') as HTMLInputElement;
@@ -94,15 +52,14 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 window.onload = () => {
     precalc();
-    simulateAsync();
-    worker.postMessage("worker start");
+    doSimulate();
 }
 
 inputElm.onchange = () => {
-    simulateAsync();
+    doSimulate();
 }
 
-async function simulateAsync() {
+function doSimulate() {
     const partsStr = inputElm.value.split(',');
     const partList: Part[] = [];
     partsStr.map((el) => {
@@ -112,14 +69,15 @@ async function simulateAsync() {
         }
     });
     
-    const simulated = await simulate(partList);
-    draw(simulated);
+    worker.postMessage({partList: partList, precalcParts: precalcParts});
+    
 }
 
-const worker = new Worker(new URL('./workers/simulate.worker.ts', import.meta.url));
+const worker = new Worker(new URL('./workers/simulate.worker.ts', import.meta.url), {type: 'module'});
 
 worker.onmessage = e => {
-    console.log("onmessage: " + e.data);
+    const simulated = e.data.simulated as PartInstance[];
+    draw(simulated);
 }
 
 // 各パーツの配置の計算を何回もやるととても時間がかかるので、事前計算する
@@ -129,8 +87,8 @@ function precalc() {
         for (let uc = 0; uc < 2; uc++) { // Uncompressed
             const compressed = !uc;
             for (let spin = 0; spin < 4; spin++) {
-                for (let pY = 0; pY < mapH; pY++) {
-                    for (let pX = 0; pX < mapW; pX++) {
+                for (let pY = 0; pY < c.mapH; pY++) {
+                    for (let pX = 0; pX < c.mapW; pX++) {
                         const partI = new PartInstance(part, part.colors[0], spin, compressed, pX, pY);
                         const memMap = partI.placedMemMap();
                         if (memMap != null && !places.some((el => JSON.stringify(el.memMap) === JSON.stringify(memMap)))) {
@@ -151,57 +109,6 @@ function precalc() {
 
 }
 
-function simulate(partList: Part[]): PartInstance[] | undefined {
-
-    const partTotal = partList.reduce((sum: number, element) => sum + PartUtils.getPartSize(element, true), 0);
-    if (partTotal > 45) {
-        return undefined;
-    }
-    
-    const partsSorted = partList.sort((a, b) => PartUtils.getPartSize(b, true) - PartUtils.getPartSize(a, true));
-        
-    const simulated = simulateStep(partsSorted, [], Array.from(new Array(mapH), () => new Array(mapW).fill(false)));
-    return simulated;
-    
-    // 再帰してシミュレート
-    function simulateStep(partList: Part[], placed: PartInstance[], memMap: boolean[][]): PartInstance[] | undefined {
-        const part = partList[0];
-        const precalcPart = precalcParts.get(part.name);
-        if (precalcPart == null) return undefined;
-
-        for (let i = 0; i < precalcPart.places.length; i++) {
-            const placeSet = precalcPart.places[i];
-            const partI = placeSet.partI;
-            const newMemMap = place(placeSet.memMap, memMap);
-            if (newMemMap != null) {
-                const newPartList = partList.concat();
-                newPartList.shift();
-
-                const newPlaced = placed.concat();
-                newPlaced.push(partI);
-                
-                if (partList.length <= 1) return newPlaced;
-
-                const next = simulateStep(newPartList, newPlaced, newMemMap);
-                if (next != null) {
-                    return next;
-                }
-            }
-        }
-        return undefined;
-    }
-
-    function place(memMapPart: boolean[][], memMapBoard: boolean[][]): boolean[][] | undefined {
-        const newMemMap: boolean[][] = Array.from(new Array(mapH), () => new Array(mapW).fill(false));
-        for (let pY = 0; pY < mapH; pY++) {
-            for (let pX = 0; pX < mapW; pX++) { // pX, pYはパーツ上の座標
-                if (memMapPart[pY][pX] && memMapBoard[pY][pX]) return undefined; // 重なるので配置不能
-                newMemMap[pY][pX] = memMapPart[pY][pX] || memMapBoard[pY][pX];
-            }
-        }
-        return newMemMap;
-    }
-}
 
 function draw(parts: PartInstance[] | undefined) {
     const blockW = 64;
@@ -230,8 +137,8 @@ function draw(parts: PartInstance[] | undefined) {
     }
 
     // グリッドを描画
-    for (let i = 0; i < mapH; i++) {
-        for (let j = 0; j < mapW; j++) {
+    for (let i = 0; i < c.mapH; i++) {
+        for (let j = 0; j < c.mapW; j++) {
             if ((i == 0 || i == 6) && (j == 0 || j == 6)) {
                 continue;
             }
@@ -246,7 +153,7 @@ function draw(parts: PartInstance[] | undefined) {
     ctx.drawImage(commandImg, mapX - blockW / 2, mapY - blockH / 2);
 
     function drawPart(partI: PartInstance) {
-        const img = partI.part.isProgram ? partI.color.img_prg : partI.color.img_pls;
+        const img = partI.part.isProgram ? partPrgImgs.get(partI.color.name) as HTMLImageElement : partPlsImgs.get(partI.color.name) as HTMLImageElement;
 
         const memMap = partI.placedMemMap();
         if (memMap == null) {
@@ -255,8 +162,8 @@ function draw(parts: PartInstance[] | undefined) {
         };
         //console.log(partI);
         //console.log(prittyPrintMemMap(memMap));
-        for (let pY = 0; pY < mapH; pY++) {
-            for (let pX = 0; pX < mapW; pX++) {
+        for (let pY = 0; pY < c.mapH; pY++) {
+            for (let pX = 0; pX < c.mapW; pX++) {
                 if (tryGetBlock(memMap, pX, pY)) {
                     const startX = mapX + pX * blockW;
                     const startY = mapY + pY * blockH;
@@ -281,7 +188,7 @@ function draw(parts: PartInstance[] | undefined) {
         }
 
         function tryGetBlock(memMap: boolean[][], x: number, y: number): boolean {
-            if (x < 0 || x >= mapW || y < 0 || y >= mapH) {
+            if (x < 0 || x >= c.mapW || y < 0 || y >= c.mapH) {
                 return false;
             } else {
                 return memMap[y][x];
